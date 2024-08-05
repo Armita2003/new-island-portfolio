9ab5e725aff42a81f48d7b66e20a6941c4165ac0
Armita2oo3 <mir.armita17@gmail.com>
2024-08-05 11:18:35 +0330

    changed the website icon and title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,14 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang='en'>
-            <Head>
+            {/* <Head title='Armita Mir Portfolio' /> */}
+
+            <Head title='Armita Mir Portfolio'>
                 <link
                     rel='icon'
                     type='image/svg+xml'
                     href='./images/favicon.ico'
                 />
-                <title>Armita Mir Portfolio</title>
+                {/* <title>Armita Mir Portfolio</title> */}
             </Head>
+
             <body>
                 <Main />
                 <NextScript />
